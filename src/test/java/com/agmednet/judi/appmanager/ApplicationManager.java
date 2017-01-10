a80354044806ab886dd3279637fafc3d0bb911b1
pshynin <pshynin@yahoo.com>
2017-01-09 20:44:57 -0500

    multi-browser capabilities selenium 3.0

--- src/test/java/com/agmednet/judi/appmanager/ApplicationManager.java
+++ src/test/java/com/agmednet/judi/appmanager/ApplicationManager.java
@@ -4,14 +4,18 @@ import com.agmednet.judi.roles.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -19,8 +23,15 @@ import java.util.concurrent.TimeUnit;
  * Created by Pasha Shynin on 8/12/2016.
  */
 public class ApplicationManager {
-    private final Properties properties;
+    private static final int SLEEP_PERIOD = 1000;
+    private static final int TIMEOUT = 30000;
+    private static ThreadLocal<WebDriver> tdriver = new ThreadLocal<WebDriver>();
     private WebDriver driver;
+
+    private final Properties properties;
+    private String browser;
+
+
     private LoginHelper loginHelper;
 
     private TrialAdmin trialAdmin;
@@ -30,7 +41,6 @@ public class ApplicationManager {
     private AdjudicatorFirst adjudicatorFirst;
     private AdjudicatorSecond adjudicatorSecond;
 
-    private String browser;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -42,15 +52,45 @@ public class ApplicationManager {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-        if (Objects.equals(browser, BrowserType.CHROME)) {
-            driver = new ChromeDriver();
-        } else if (Objects.equals(browser, BrowserType.FIREFOX)) {
-            driver = new FirefoxDriver();
-        } else if (Objects.equals(browser, BrowserType.IE)) {
-            driver = new InternetExplorerDriver();
+        if (tdriver.get() != null) {
+            driver = tdriver.get();
+            return;
+        }
+
+        switch (browser) {
+            case BrowserType.FIREFOX: {
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability(FirefoxDriver.MARIONETTE, false);
+                driver = new FirefoxDriver(caps);
+                tdriver.set(driver);
+                break;
+            }
+            case BrowserType.CHROME: {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("start-fullscreen");
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability(ChromeOptions.CAPABILITY, options);
+                driver = new ChromeDriver();
+                tdriver.set(driver);
+                break;
+            }
+            case BrowserType.IE:
+                driver = new InternetExplorerDriver();
+                tdriver.set(driver);
+                break;
+            case BrowserType.SAFARI: {
+                SafariOptions options = new SafariOptions();
+                options.setUseCleanSession(true);
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability(SafariOptions.CAPABILITY, options);
+                driver = new SafariDriver();
+                tdriver.set(driver);
+                break;
+            }
         }
 
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        new WebDriverWait(this.driver, TIMEOUT, SLEEP_PERIOD);
         driver.get(properties.getProperty("web.baseUrl"));
         driver.manage().window().maximize();
 
@@ -62,6 +102,14 @@ public class ApplicationManager {
         reviewer = new Reviewer(driver);
         adjudicatorFirst = new AdjudicatorFirst(driver);
         adjudicatorSecond = new AdjudicatorSecond(driver);
+    }
+
+    public void stop() {
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(() -> {
+                    driver.quit();
+                    driver = null;
+                }));
     }
 
     public LoginHelper loginAs() {
@@ -102,9 +150,5 @@ public class ApplicationManager {
 
     String getProperty(String key) {
         return properties.getProperty(key);
-    }
-
-    public void stop() {
-        driver.quit();
     }
 }
