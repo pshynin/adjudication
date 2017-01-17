a383ba396d3666870ead4838c090044ef2b54b47
pshynin <pshynin@yahoo.com>
2017-01-17 17:32:48 -0500

    test suit Enets #1

---
+++ src/test/java/com/agmednet/judi/appmanager/NavigationHelper.java
@@ -0,0 +1,27 @@
+package com.agmednet.judi.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by pshynin on 1/12/17.
+ */
+public class NavigationHelper extends HelperBase {
+    private static final String URL = "https://test05.agmednet.net";
+    private static final String LOGIN = "//div[@id='userwidget']//a[.='Login']";
+    private static final String LOGOUT = "//*[@href='/logout.seam' and contains(text(), 'Logout')]";
+
+
+    NavigationHelper(WebDriver driver) {
+        super(driver);
+    }
+
+    public void loginPage() {
+        driver.get(URL);
+ //       driver.findElement(By.xpath(LOGIN)).click();
+    }
+
+    public void logoutPage() {
+        driver.findElement(By.xpath(LOGOUT)).click();
+    }
+}
---
+++ src/test/java/com/agmednet/judi/roles/CommitteeChair.java
@@ -0,0 +1,14 @@
+package com.agmednet.judi.roles;
+
+import com.agmednet.judi.appmanager.HelperBase;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by pshynin on 1/15/17.
+ */
+public class CommitteeChair extends HelperBase{
+
+    public CommitteeChair(WebDriver driver) {
+        super(driver);
+    }
+}
