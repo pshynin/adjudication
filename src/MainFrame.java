394d506bbbddbe8468cf70863c147b6aea8932a9
Pavel <pshynin@yahoo.com>
2018-07-29 16:20:25 -0400

    game frame added

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,16 +1,17 @@
 import javax.swing.*;
 
-public class PlayWindow extends JFrame {
+public class MainFrame extends JFrame {
 
-    public PlayWindow() {
+    public MainFrame() {
         setTitle("Snake");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(320, 345);
         setLocation(400, 400);
+        add(new GameFrame());
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        PlayWindow window = new PlayWindow();
+        MainFrame window = new MainFrame();
     }
 }
