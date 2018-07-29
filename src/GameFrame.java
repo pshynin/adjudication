de40caafd82c27fb09338db6c89e40dcff9b8a4b
Pavel <pshynin@yahoo.com>
2018-07-29 16:27:04 -0400

    added game parameters

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JPanel {
+    private final int SIZE = 320;
+    private final int DOT_SIZE = 16;
+    private final int ALL_DOTS = 400;
+    private Image dot;
+    private Image apple;
+    private int appleX;
+    private int appleY;
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+    private int dots;
+    private Timer timer;
+    private boolean left = false;
+    private boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    private boolean inGame = true;
+
+    public GameFrame() {
+    }
 }
