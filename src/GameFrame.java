5afe2f87ed8fe0946b572495929c034f91670572
Pavel <pshynin@yahoo.com>
2018-07-31 07:32:58 -0400

    added images

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,8 +5,8 @@ public class GameFrame extends JPanel {
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
-    private Image dot;
     private Image apple;
+    private Image dot;
     private int appleX;
     private int appleY;
     private int[] x = new int[ALL_DOTS];
@@ -20,5 +20,14 @@ public class GameFrame extends JPanel {
     private boolean inGame = true;
 
     public GameFrame() {
+        setBackground(Color.BLACK);
+        loadImager();
+    }
+
+    public void loadImager() {
+        ImageIcon appleImage = new ImageIcon("apple.png");
+        apple = appleImage.getImage();
+        ImageIcon dotImage = new ImageIcon("dot.png");
+        dot = dotImage.getImage();
     }
 }
